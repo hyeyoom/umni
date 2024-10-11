795bd3a45888bd4c10ebf141a05b024a14bc7cb1
hyeyoom <neoul_chw@icloud.com>
2024-10-11 17:32:03 +0900

    disabled output: export option to enable dynamic api

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 export function GET(request: Request) {
     console.log(request.url)
     return new Response(`Hello from ${process.env.VERCEL_REGION} [${request.url}]`);
